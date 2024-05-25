697e4e6b3b4fac2d827e0e7122556677e39d20fe
mbr3d4 <93344115+mbr3d4@users.noreply.github.com>
2024-05-25 12:03:32 -0300

    Create jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Preparação') {
+            steps {
+                // Aqui você pode adicionar etapas para preparar o ambiente
+                echo 'Preparando o ambiente...'
+            }
+        }
+        stage('Build') {
+            steps {
+                // Aqui você pode adicionar etapas para compilar o seu código
+                echo 'Compilando o código...'
+            }
+        }
+        stage('Testes') {
+            steps {
+                // Aqui você pode adicionar etapas para executar testes automatizados
+                echo 'Executando testes...'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                // Aqui você pode adicionar etapas para implantar sua aplicação
+                echo 'Implantando a aplicação...'
+            }
+        }
+    }
+    
+    post {
+        always {
+            // Etapa executada sempre, independentemente do resultado das etapas anteriores
+            echo 'Finalizando a execução da pipeline...'
+        }
+        success {
+            // Etapa executada apenas se a pipeline for bem-sucedida
+            echo 'Pipeline executada com sucesso!'
+        }
+        failure {
+            // Etapa executada apenas se a pipeline falhar
+            echo 'A pipeline falhou!'
+        }
+    }
+}
